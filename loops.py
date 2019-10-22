7c0376c7bfd58f3f22563ddc5a8213ba41acede7
unknown <khanafsaan11@gmail.com>
2019-10-23 03:25:27 +0530

    for, while,range

---
+++ loops.py
@@ -0,0 +1,44 @@
+# print first 10 numbers
+
+# for loop
+
+languages = ['java','python','kotlin','c','C++']
+name = 'afsan'
+
+for i in languages:
+    print(i)
+
+for char in name:
+    print(char)
+    print(char)
+
+
+# range
+
+for i in range (len(languages)):
+    print(languages[i])
+
+for m in range(1,5):
+
+    print(m)
+    
+for n in range(10,21,2):
+    print(n)
+
+# While loop
+
+i=0
+while ( i < len(languages)):
+    print(languages[i])
+    i = i+1
+    
+
+j=0
+p=-10
+while j <5 or p<0:
+    print(j)
+    print(p)
+    j = j+1
+    p=p+1
+
+    
