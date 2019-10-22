3e637cc6bc6e9fc31e5c461be3cab712d20987bb
unknown <khanafsaan11@gmail.com>
2019-10-23 03:24:49 +0530

    Condition statement

---
+++ Condition.py
@@ -0,0 +1,12 @@
+# if condition
+
+number = int(input("enter any number of your choice:-"))
+
+if number < 0:
+    print("number is less than zero")
+elif number ==0:
+    print("number is equal to zero")
+else:
+    print("number is more than zero")
+
+
