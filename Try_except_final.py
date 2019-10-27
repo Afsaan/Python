9f218027817364ceddbddd4b7fdf674f58af5267
unknown <khanafsaan11@gmail.com>
2019-10-28 02:37:39 +0530

    try_except_finally

---
+++ Try_except_final.py
@@ -0,0 +1,12 @@
+
+try:
+    c= 4
+    d = int(input("please enter a number of yout choice"))
+    a=10/d
+    print("hello world")
+except ZeroDivisionError:
+    print (" number cannot be divided by zero")
+except NameError:
+    print("name error hia madarchod..theek se dekh")
+    
+
