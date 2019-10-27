f2c8790e8ad50a4b4ea925b3462ac12d8fd07647
unknown <khanafsaan11@gmail.com>
2019-10-28 03:43:45 +0530

    model_import file

---
+++ model_import.py
@@ -0,0 +1,19 @@
+# you can import the other files 
+
+# importing the fucntions
+import file as f
+
+print(f.factorial(2))
+
+# you can also import the attribute also
+
+print(f.X)
+
+# you can call the particular attribute or the method (Function)
+
+from file import factorial ,sum
+
+print(factorial(6))
+print(sum(2,3))
+
+
